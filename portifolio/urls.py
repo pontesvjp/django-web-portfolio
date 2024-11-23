521869f3b43f32a3d4d8dd855ad6be7287447f6f
pontesvjp <pontes.vjp@gmail.com>
2024-11-23 12:22:12 -0300

    deploy 4

--- portifolio/urls.py
+++ portifolio/urls.py
@@ -3,7 +3,8 @@ from portifolio.views import index, contact, resume, projects
 
 urlpatterns = [
     path('', index, name='index'),
-    path('contact', contact, name='contact'),
-    path('resume', resume, name='resume'),
-    path('projects', projects, name='projects'),
+    path('contact/', contact, name='contact'),
+    path('resume/', resume, name='resume'),
+    path('projects/', projects, name='projects'),
+
 ]
