b74163d881ae5b6dd44b063724d0f9ccf1ab7e76
pontesvjp <pontes.vjp@gmail.com>
2025-04-30 20:01:18 -0300

    eh noisxxxxx

--- setup/settings.py
+++ setup/settings.py
@@ -19,8 +19,7 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['*'] 
-
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -43,7 +42,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    "whitenoise.middleware.WhiteNoiseMiddleware", 
+    "whitenoise.middleware.WhiteNoiseMiddleware",
 ]
 
 ROOT_URLCONF = 'setup.urls'
